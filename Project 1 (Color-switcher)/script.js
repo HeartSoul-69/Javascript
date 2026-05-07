3d9214f21aabfa01faff7f9511ee1eb3b542e319
HeartSoul-69 <anurag355singh@gmail.com>
2026-05-07 11:33:38 +0530

    added and updated files

--- Project 1 (Color-switcher)/script.js
+++ Project 1 (Color-switcher)/script.js
@@ -2,18 +2,19 @@ const a = document.querySelectorAll(".button")
 const b = document.querySelector("body")
 
 a.forEach(function (button) {
-    button.addEventListener("click", function (e) {
-        if(e.target.id == "grey") {
-            b.style.backgroundColor = "grey";
+    button.addEventListener("click" , function(e){
+        if (e.target.id === "grey") {
+            b.style.backgroundColor = e.target.id
         }
-        if(e.target.id == "yellow") {
-            b.style.backgroundColor = e.target.id;
+        if (e.target.id === "yellow") {
+            b.style.backgroundColor = e.target.id
         }
-        if(e.target.id == "white") {
-            b.style.backgroundColor = e.target.id;
+        if (e.target.id === "white") {
+            b.style.backgroundColor = e.target.id
         }
-        if(e.target.id == "green") {
-            b.style.backgroundColor = e.target.id;
+        if (e.target.id === "green") {
+            b.style.backgroundColor = e.target.id
         }
     })
 })
+
--- Project 2 (BMI)/script.js
+++ Project 2 (BMI)/script.js
@@ -1,22 +1,23 @@
 const form = document.querySelector("form")
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit" , function (e) {
     e.preventDefault()
 
-    const height = (document.querySelector("#height").value)
-    const weight = (document.querySelector("#weight").value)
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
     const res = document.querySelector("#result")
 
     if (height === " " || height < 0 || isNaN(height)) {
-        res.innerHTML = "Please enter a valid height"
+        res.innerHTML =     `Please enter a valid height ${height}`
     }
-    else if (weight === " " || height < 0 || isNaN(weight)){
-        res.innerHTML = "Please enter a valid weight"
+    else if (weight === " " || weight < 0  || isNaN(weight)){
+        res.innerHTML = `Please enter a valid weight ${weight}`
     }
     else {
-        const BMI = (weight/((height*height)/10000)).toFixed(2)
-        res.innerHTML = BMI 
+        const BMI  = (weight/((height*height)/10000)).toFixed(2)
+        res.innerHTML = `<span>${BMI}</span>`
     }
+   
 })
 
 
